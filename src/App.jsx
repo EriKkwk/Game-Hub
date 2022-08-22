89d3108d1502b016f661b31b24c1e8b846b5b3c7
EriKkwk <ersahakyanit@gmail.com>
2022-08-22 21:30:17 +0400

    Game Hub

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Game3 from './components/Game 3/game3'
 import './App.css'
 
 function App() {
-  return <div>
-
+  return <div className='App'>
+    <Game3 />
   </div>
 }
 
-export default App
+export default App;
---
+++ src/components/Game 3/game3.jsx
@@ -0,0 +1,13 @@
+import './game3.css'
+
+function Game3(){
+    return <div className='rsp-container'>
+        <div className='heading'>
+            <span className="Rock">Rock</span>
+            <span className='Scissors'>Scissors</span>
+            <span className='Paper'>Paper</span>
+        </div>
+    </div>
+}
+
+export default Game3;
