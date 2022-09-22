e0edb96c24d1a52577643fb62521ac4154f15089
EriKkwk <ersahakyanit@gmail.com>
2022-09-22 19:17:05 +0400

    wwweqewqweasd

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-<<<<<<< HEAD
 ReactDOM.createRoot(document.getElementById('root')).render(
-=======
-ReactDOM.createRoot(document.getElementById('root')).render(
->>>>>>> 58e2196642ee3d6e1bbcd3a20254d4062a26392b
     <React.StrictMode>
       <App />
     </React.StrictMode>
-    )
+)
