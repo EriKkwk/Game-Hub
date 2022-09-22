6d2b85e4ce6cc3f7123f24add1a2e92a5a768693
EriK <103878723+EriKkwk@users.noreply.github.com>
2022-09-22 18:58:48 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-const http = require('node:http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(
-    ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
     )
-  );
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
